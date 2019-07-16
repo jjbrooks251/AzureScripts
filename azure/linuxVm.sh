9d02cd5d9b13f190502b383c519d94fdcf0bb668
Josh Brooks <50910150+jjbrooks251@users.noreply.github.com>
2019-07-16 11:26:17 +0100

    Update linuxVm.sh

--- azure/linuxVm.sh
+++ azure/linuxVm.sh
@@ -1,9 +1,10 @@
 echo "Enter the name of the vm(s) you wish to add to the group, seperate names by a space"
 read vmName
 
+echo "enter an existing group name you wish to add the vm(s) too"
+read groupName
+
 for name in $vmName
 do
 az vm create -g ${groupName} -n $name --image UbuntuLTS --generate-ssh-keys 
 done
-
-export vmName=$vmName
