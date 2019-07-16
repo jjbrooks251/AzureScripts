cbe999f1a135de0f2c3968e4f2ce98c276687f45
Josh Brooks <50910150+jjbrooks251@users.noreply.github.com>
2019-07-16 11:27:36 +0100

    Update ip.sh

--- azure/ip.sh
+++ azure/ip.sh
@@ -1,3 +1,8 @@
+echo "Enter the name of the vm you want the ip address for"
+read vm
+
+echo "Enter the group the vm belongs too"
+read groupName
+
 ip=$(az vm list-ip-addresses -g ${groupName} -n ${vm} | grep ipAddress | cut -d '"' -f4)
 echo $ip
-export ip=$ip
